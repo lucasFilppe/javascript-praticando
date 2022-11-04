2dee5047d223c569170d4148642dd00fa3a5a950
lucasFilppe <lucasfilipe2323@icloud.com>
2022-11-04 02:16:49 -0300

    initaial commit

---
+++ converteGraus/index.js
@@ -0,0 +1,12 @@
+//captura evento do formularip
+const form = document.querySelector('#form-graus')
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        //pega input
+        const inputGrausCelsius = document.querySelector('#grausCelsius');
+
+        
+
+    });
