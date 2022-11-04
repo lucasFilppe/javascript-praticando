6169b03138e427fab8d75a19cfda064e48ab7bc1
lucasFilppe <lucasfilipe2323@icloud.com>
2022-11-04 14:29:16 -0300

    criando funcionalidades

--- converteGraus/index.js
+++ converteGraus/index.js
@@ -7,6 +7,20 @@ const form = document.querySelector('#form-graus')
         //pega input
         const inputGrausCelsius = document.querySelector('#grausCelsius');
 
-        
+        const graus = Number(inputGrausCelsius.value) //valor do input
 
+        //
+        const f =  (graus *  (9/5)) + 32;
+        
+        //se graus nao for valido
+        if(!graus) {
+            setGraus(`Temperatunra invalida`)
+        }else {
+            setGraus(`Temperatura valida ${f}`)
+        }
     });
+
+    function setGraus(msg) {
+        const resul =  document.querySelector('.resultado')
+        resul.innerHTML = msg
+    }
