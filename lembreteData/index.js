ba761446a0446a97867b2f1a73d29af4a6795166
lucasFilppe <lucasfilipe2323@icloud.com>
2022-11-09 13:22:12 -0300

    criando funcionalidade para nome do mes

--- lembreteData/index.js
+++ lembreteData/index.js
@@ -1,34 +1,95 @@
-function getDiaSemanatexto(diaSemena) {
-    let diaDaSemanaTxt;//só exist na funçao
+const h1 = document.querySelector(".container h1");
+const data = new Date();
+
+function getDiaSemanatexto(diaSemana) {
+  let diaDaSemanaTxt; //só exist na funçao
   switch (diaSemana) {
     case 0:
       diaDaSemanaTxt = "Domingo";
-      return diaDaSemanaTxt
+      return diaDaSemanaTxt;
     case 1:
       diaDaSemanaTxt = "Segunda";
-      return diaDaSemanaTxt
+      return diaDaSemanaTxt;
     case 2:
       diaDaSemanaTxt = "terça";
-      return diaDaSemanaTxt
+      return diaDaSemanaTxt;
     case 3:
       diaDaSemanaTxt = "quarta";
-      return diaDaSemanaTxt
+      return diaDaSemanaTxt;
     case 4:
       diaDaSemanaTxt = "quinta";
-      return diaDaSemanaTxt
+      return diaDaSemanaTxt;
     case 5:
       diaDaSemanaTxt = "sexta";
-      return diaDaSemanaTxt
+      return diaDaSemanaTxt;
     case 6:
       diaDaSemanaTxt = "sabado";
-      return diaDaSemanaTxt
+      return diaDaSemanaTxt;
   }
 }
 
-const data = new Date();
-const diaSemana = data.getDay();
-const diaDaSemanaTxt = getDiaSemanatexto(diaSemana)
-console.log(diaSemana, diaDaSemanaTxt);
+function getMesesDoAno(numeroMes) {
+  let nomeDoMes;
 
-const h1 = document.querySelector(".container h1");
-h1.innerHTML = "so pra ve";
+  switch (numeroMes) {
+    case 1:
+      nomeDoMes = "janeiro";
+      return nomeDoMes;
+
+    case 2:
+      nomeDoMes = "fevereiro";
+      return nomeDoMes;
+
+    case 3:
+      nomeDoMes = "março";
+      return nomeDoMes;
+
+    case 4:
+      nomeDoMes = "abril";
+      return nomeDoMes;
+
+    case 5:
+      nomeDoMes = "maio";
+      return nomeDoMes;
+
+    case 61:
+      nomeDoMes = "junho";
+      return nomeDoMes;
+
+    case 7:
+      nomeDoMes = "julho";
+      return nomeDoMes;
+
+    case 8:
+      nomeDoMes = "agosto";
+      return nomeDoMes;
+
+    case 9:
+      nomeDoMes = "setembro";
+      return nomeDoMes;
+
+    case 10:
+      nomeDoMes = "outubro";
+      return nomeDoMes;
+
+    case 11:
+      nomeDoMes = "novembro";
+      return nomeDoMes;
+
+    case 12:
+      nomeDoMes = "dezembro";
+      return nomeDoMes;
+  }
+}
+
+function criaData(data) {
+  const diaSemana = data.getDay()
+  const nomeDoMes = data.month()
+
+  const nomeDia = getDiaSemanatexto(diaSemana)
+  const nomeMes = getMesesDoAno(nomeDoMes)
+
+  console.log(nomeDia, nomeMes)
+}
+
+criaData()
